3bb3f583a0f2dbe86bc6fbd6ae4274419171b540
AfsalHashim <138692053+CyberTG@users.noreply.github.com>
2025-11-11 23:09:13 +0530

    Update handle.py

--- handle.py
+++ handle.py
@@ -80,7 +80,16 @@ async def admin_menu_handler(cyber, query):
         await query.answer("You are not authorized. Only the admin can open this menu.", show_alert=True)
         return
 
-    about_text = ADMIN_TEXT
+    about_text = """𝗧𝗛𝗜𝗦 𝗜𝗦 𝗠𝗬 𝗔𝗗𝗠𝗜𝗡 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦
+
+/setskip - To Skip Indexing File range
+/broadcast - To broadcast Message To users 
+/deleteall - Delete All Files From Database 
+/deletefile  - Delete a replyed File from database 
+/total - Get Total Uses and Files 
+/imdb - To Turn on and off Imdb 
+/fsub - To Set Force Subscribe 
+/autodel - To Set Autodelete Time"""
     ADMIN_KEYBOARD = InlineKeyboardMarkup([
         [InlineKeyboardButton("🔙 Back", callback_data="cyber|"),
          InlineKeyboardButton("Close", callback_data="close")]
@@ -104,8 +113,23 @@ async def admin_menu_handler(cyber, query):
 
 @cyber.on_callback_query(filters.regex(r"^about\|"))
 async def about_menu_handler(cyber, query):
-    about_text = ABOUT_TEXT
-    reply_markup = ABOUT_KEYBOARD
+    about_text = """ <blockquote>
+‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://t.me/TGxSeriesBot'>Sᴇʀɪᴇs Bᴏᴛ</a>
+‣ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>
+‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/CyberTGx'>ᴄʏʙᴇʀ ᠰ ᴛɢ</a>
+‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a>
+‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a>
+‣ ᴘʀɪᴍᴀʀʏ ᴅʙ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a>
+‣ sᴇᴄᴏɴᴅᴀʀʏ ᴅʙ : <a href='https://www.freesqldatabase.com/'>sᴏ̨ʟ ᴅʙ</a>
+‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://t.me/quickfastt'>ǫᴜɪᴄᴋ ꜰᴀsᴛ</a></b></blockquote>"""
+    reply_markup = ABOUT_KEYBOARD = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton("💻 Source Code", url="https://github.com/CyberTG/SeriesFilterBot")
+        ],[
+            InlineKeyboardButton("🔰 Home", callback_data="cyber|"),
+            InlineKeyboardButton("❤️‍🩹 Developer", url="https://t.me/CyberTGx")
+        ],
+    ])
     try:
         await query.message.edit_caption(
             about_text, reply_markup=reply_markup, parse_mode=enums.ParseMode.HTML
@@ -121,39 +145,3 @@ async def about_menu_handler(cyber, query):
         except Exception:
             pass
     await query.answer("Showing about information.")
-
-
-
-ADMIN_TEXT ="""𝗧𝗛𝗜𝗦 𝗜𝗦 𝗠𝗬 𝗔𝗗𝗠𝗜𝗡 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦
-
-/setskip - To Skip Indexing File range
-/broadcast - To broadcast Message To users 
-/deleteall - Delete All Files From Database 
-/deletefile  - Delete a replyed File from database 
-/total - Get Total Uses and Files 
-/imdb - To Turn on and off Imdb 
-/fsub - To Set Force Subscribe 
-/autodel - To Set Autodelete Time
-"""
-
-ABOUT_TEXT = """ <blockquote>
-‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://t.me/TGxSeriesBot'>Sᴇʀɪᴇs Bᴏᴛ</a>
-‣ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>
-‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/CyberTGx'>ᴄʏʙᴇʀ ᠰ ᴛɢ</a>
-‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a>
-‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a>
-‣ ᴘʀɪᴍᴀʀʏ ᴅʙ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a>
-‣ sᴇᴄᴏɴᴅᴀʀʏ ᴅʙ : <a href='https://www.freesqldatabase.com/'>sᴏ̨ʟ ᴅʙ</a>
-‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://t.me/quickfastt'>ǫᴜɪᴄᴋ ꜰᴀsᴛ</a></b></blockquote>"""
-
-ABOUT_KEYBOARD = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton("💻 Source Code", url="https://github.com/CyberTG/SeriesFilterBot")
-        ],
-        [
-            InlineKeyboardButton("🔰 Home", callback_data="cyber|"),
-            InlineKeyboardButton("❤️‍🩹 Developer", url="https://t.me/CyberTGx")
-        ],
-    ]
-)
